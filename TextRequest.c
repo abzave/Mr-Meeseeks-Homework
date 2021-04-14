e6c9078989afaa47b5f2ad2d3d2e7e00ed622d0e
Abraham Meza <abzave@gmail.com>
2021-04-13 19:52:23 -0600

    Chaos control plan New control plan for chaos condition

--- TextRequest.c
+++ TextRequest.c
@@ -1,6 +1,8 @@
 #include "TextRequest.h"
 
-
+/**
+ * Ask the user for how to determine the dificulty of the task
+ */
 float getDificulty() {
     char* options[2];
     options[0] = "Ingresar dificultad.";
@@ -20,4 +22,34 @@ float getUserDificulty() {
 
 float getRandomDificulty() {
     return (rand() / 100) % 100;
+}
+
+/**
+ * Get the time elapsed between 2 time marks in seconds
+ * @param initTime {clock_t} first marker
+ * @param endTime {clock_t} second marker
+ */
+double timediff(clock_t initTime, clock_t endTime) {
+    double elapsed;
+    elapsed = ((double)endTime - initTime) / CLOCKS_PER_SEC * 1000000.0;
+    timeElasep += elapsed;
+    return elapsed;
+}
+
+/**
+ * Check if the time elapsed is enough to declare global chaos
+ */
+int systemCollapsed(){
+    return timeElasep >= timeLimitInSeconds;
+}
+
+/**
+ * We have global chaos and now we need to kill all our chidren
+ * Here for the chaos!
+ */
+void declareGlobalChaos() {
+    printf("La solicitud es muy dificil hay caos global :S\n");
+    for (int child = 0; child < lastChild; child++) {
+        kill(children[child], SIGKILL);
+    }
 }
--- TextRequest.h
+++ TextRequest.h
@@ -5,14 +5,23 @@
 #include <sys/types.h>
 #include <time.h>
 #include <stdlib.h>
+#include <signal.h>
 
 #include "CLI/interface.h"
 
 #define USER_INPUT          1
 #define LUCK_DIFICULTY      2
 
+int timeLimitInSeconds = 300;
+double timeElasep = 0;
+pid_t children[300];        // TODO: Change magic constant
+int lastChild = 0;
+
 float getDificulty(void);
 float getUserDificulty(void);
 float getRandomDificulty(void);
+
+double timediff(clock_t, clock_t);
+void declareGlobalChaos();
 
 #endif
